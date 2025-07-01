3877a33af347ca73e7ab2991f31330dfbdadfe1e
Liam Paknia <106473092+sajjadpaknia@users.noreply.github.com>
2025-07-01 20:22:20 +0330

    feat/add padding app container

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -5,7 +5,7 @@ import React, { PropsWithChildren } from 'react'
 
 const AppContainer = ({ children }: PropsWithChildren) => {
     return (
-        <div className='max-w-[800px] mx-auto h-screen'>
+        <div className='max-w-[800px] mx-auto h-screen px-8'>
             {children}
         </div>
     )
